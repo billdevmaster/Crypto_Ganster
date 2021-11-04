842875dc302a546aa59dcee78ed57b89a4dfc6b3
billdevmaster71 <billdevmaster@gmail.com>
2021-11-04 14:38:53 +0000

    1

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ async function fetchAccountData() {
 	contract = new web3.eth.Contract(abi, "0x643e08b21a1DA1a00f97dd974931D09abb1C7c36");
 }
 
-function init(){
+function init() {
 	const providerOptions = {
    walletconnect: {
      package: WalletConnectProvider,
